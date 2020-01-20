41f45b4aa7d0a91c77eeee34294519d0ba7a1e48
pandeysp8 <59777007+pandeysp8@users.noreply.github.com>
2020-01-19 22:21:22 -0500

    Add files via upload

---
+++ Assignment1/src/Business/Product.java
@@ -0,0 +1,314 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author Swati
+ */
+import java.util.Date;
+public class Product {
+    private String Firstname;
+    private String Lastname;
+    private String Phonenum;
+    private String SSN;
+    private String DOB;
+    private String age;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String B1name;
+    private String B1type;
+    private String Bal1;
+    private String AcNum1;
+    private String RNum1;
+    private String B2name;
+    private String B2type;
+    private String Bal2;
+    private String AcNum2;
+    private String RNum2;
+    //private String DL;
+    private String picSet;
+    private String MNum;
+    private String Al1;
+    private String Al2;
+    private String Al3;
+    private String DLNum;
+    private String DOI;
+    private String DOE;
+    private String Blood;
+    private String Height;
+    private String Weight;
+
+    public String getHeight() {
+        return Height;
+    }
+
+    public void setHeight(String Height) {
+        this.Height = Height;
+    }
+
+    public String getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(String Weight) {
+        this.Weight = Weight;
+    }
+
+    public String getDLNum() {
+        return DLNum;
+    }
+
+    public void setDLNum(String DLNum) {
+        this.DLNum = DLNum;
+    }
+
+    public String getDOI() {
+        return DOI;
+    }
+
+    public void setDOI(String DOI) {
+        this.DOI = DOI;
+    }
+
+    public String getDOE() {
+        return DOE;
+    }
+
+    public void setDOE(String DOE) {
+        this.DOE = DOE;
+    }
+
+    public String getBlood() {
+        return Blood;
+    }
+
+    public void setBlood(String Blood) {
+        this.Blood = Blood;
+    }
+    
+    public Object Integer;
+
+    public String getFirstname() {
+        return Firstname;
+    }
+
+    public void setFirstname(String Firstname) {
+        this.Firstname = Firstname;
+    }
+
+    public String getLastname() {
+        return Lastname;
+    }
+
+    public void setLastname(String Lastname) {
+        this.Lastname = Lastname;
+    }
+
+    public String getPhonenum() {
+        return Phonenum;
+    }
+
+    public void setPhonenum(String Phonenum) {
+        this.Phonenum = Phonenum;
+    }
+
+    public String getSSN() {
+        return SSN;
+    }
+
+    public void setSSN(String SSN) {
+        this.SSN = SSN;
+    }
+
+   public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+    
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getB1name() {
+        return B1name;
+    }
+
+    public void setB1name(String B1name) {
+        this.B1name = B1name;
+    }
+
+    public String getB1type() {
+        return B1type;
+    }
+
+    public void setB1type(String B1type) {
+        this.B1type = B1type;
+    }
+
+    public String getBal1() {
+        return Bal1;
+    }
+
+    public void setBal1(String Bal1) {
+        this.Bal1 = Bal1;
+    }
+
+    public String getAcNum1() {
+        return AcNum1;
+    }
+
+    public void setAcNum1(String AcNum1) {
+        this.AcNum1 = AcNum1;
+    }
+
+    public String getRNum1() {
+        return RNum1;
+    }
+
+    public void setRNum1(String RNum1) {
+        this.RNum1 = RNum1;
+    }
+
+    public String getB2name() {
+        return B2name;
+    }
+
+    public void setB2name(String B2name) {
+        this.B2name = B2name;
+    }
+
+    public String getB2type() {
+        return B2type;
+    }
+
+    public void setB2type(String B2type) {
+        this.B2type = B2type;
+    }
+
+    public String getBal2() {
+        return Bal2;
+    }
+
+    public void setBal2(String Bal2) {
+        this.Bal2 = Bal2;
+    }
+
+    public String getAcNum2() {
+        return AcNum2;
+    }
+
+    public void setAcNum2(String AcNum2) {
+        this.AcNum2 = AcNum2;
+    }
+
+    public String getRNum2() {
+        return RNum2;
+    }
+
+    public void setRNum2(String RNum2) {
+        this.RNum2 = RNum2;
+    }
+
+    public String getPicSet() {
+        return picSet;
+    }
+
+    public void setPicSet(String picSet) {
+        this.picSet = picSet;
+    }
+
+    public Object getInteger() {
+        return Integer;
+    }
+
+    public void setInteger(Object Integer) {
+        this.Integer = Integer;
+    }
+
+    
+
+    
+   
+
+
+    public String getMNum() {
+        return MNum;
+    }
+
+    public void setMNum(String MNum) {
+        this.MNum = MNum;
+    }
+
+    public String getAl1() {
+        return Al1;
+    }
+
+    public void setAl1(String Al1) {
+        this.Al1 = Al1;
+    }
+
+    public String getAl2() {
+        return Al2;
+    }
+
+    public void setAl2(String Al2) {
+        this.Al2 = Al2;
+    }
+
+    public String getAl3() {
+        return Al3;
+    }
+
+    public void setAl3(String Al3) {
+        this.Al3 = Al3;
+    }
+    
+    
+            
+    
+}
